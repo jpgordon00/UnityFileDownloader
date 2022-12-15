9a87a55ceab54e58461a95fc09a9cb16aacd7af5
Jacob Gordon <62991719+jpgordon00@users.noreply.github.com>
2022-12-15 08:53:18 -0800

    Update UnitTests.cs

--- Examples/UnitTests.cs
+++ Examples/UnitTests.cs
@@ -10,29 +10,33 @@ using System.IO;
 /// </summary>
 public class UnitTests : MonoBehaviour
 {
-
-     async void Download()
+    async void Download()
     {
-        Debug.Log("00000000");
         UnityFileDownloader ufd = new UnityFileDownloader(new string[] {
             "https://wallpaperaccess.com/full/87731.jpg",
             "https://wallpaperaccess.com/full/1126085.jpg",
             "https://wallpaperaccess.com/full/1308917.jpg",
+            "https://wallpaperaccess.com/full/281585.jpg",
         });
+        ufd.RequestHeaders = new Dictionary<string, string>{
+          ["key"] = "value"
+        };
         ufd.AbandonOnFailure = true;
         ufd.ContinueAfterFailure = false;
-        ufd.MaxConcurrency = 2; // Unity does impose an asyncronous limit so I'd keep this under 10 
+        ufd.MaxConcurrency = 3;
+        ufd.TryMultipartDownload = true; // false to disable multipart
         ufd.OnDownloadSuccess += (string uri) => {
             Debug.Log("Downloaded " + uri + "!");
+            IDownloadFulfiller idf = ufd.GetFulfiller(uri);
+            Debug.Log("This download was " + (ufd.MultipartDownload ? "" : "NOT ") + "downloaded in multiparts.");
 
-            if (true)
-            {   // dummy
-                //ufd.Cancel();
+            if (true) { // dummy
+                ufd.Cancel();
             }
         };
-        ufd.OnDownloadChunkedSucces += (uri) => {
-            IDownloadFulfiller idf = ufd.GetFulfiller(uri);
-            Debug.Log("Progress for " + uri + " is " + ufd.GetProgress(uri) + ", and " + idf.BytesDownloaded + " bytes downloaded, " + idf.MegabytesDownloadedPerSecond + " mb/s.");
+        // only if multipart is enabled for this uri
+        ufd.OnDownloadChunkedSucces += (uri) {
+            Debug.Log("Progress for " + uri + " is " + ufd.GetProgress(uri));
         };
         ufd.OnDownloadsSuccess += () => {
             Debug.Log("Downloaded all files. (inline func)");
@@ -41,15 +45,15 @@ public class UnitTests : MonoBehaviour
         {
             Debug.Log($"ErrorCode={errorCode}, EM={errorMsg}, URU={uri}");
         };
-        Debug.Log(ufd.MultipartChunkSize); // amount of bytes used by each chunked https request 
         await ufd.Download();
         Debug.Log("Downloaded all files. (post-awaitable Download invokation)");
         Debug.Log("MB/S = " + ufd.MegabytesDownloadedPerSecond);
-        Debug.Log(ufd.DownloadPath);
     }
+   
+ void Start() {
+   Download();
+ }
 
-void Start()
-    {
-        Download();
-    }
+}
+
 }
